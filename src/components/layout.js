99cb709a75cd3c1ef2cfa2624fa2277f6366d0ce
Praveen Kumar Gorakala <praveenasvenom@gmail.com>
2019-10-17 00:13:45 +0530

    init commit again

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,16 @@
-import React, {Fragment} from "react"
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import {IoMdHome,IoMdInformationCircleOutline,IoIosSettings,IoMdImage,IoMdMail} from "react-icons/io"
 
-import NavCircle from "./NavCircle"
-import "../styles/main.scss"
-import { Link } from "gatsby"
+import Header from "./header"
+import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -19,18 +24,24 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <Fragment>
-      <NavCircle>
-          <Link className="noBgHover" to="/#home"><IoMdHome/></Link>
-          <Link className="noBgHover" to="/#about"><IoMdInformationCircleOutline/></Link>
-          <Link className="noBgHover" to="/#services"><IoIosSettings/></Link>
-          <Link className="noBgHover" to="/portfolio"><IoMdImage/></Link>
-          <Link className="noBgHover" to="/#connect"><IoMdMail/></Link>
-      </NavCircle>
-      <div id="page">
-        <main>{children}</main>    
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </div>
-    </Fragment>
+    </>
   )
 }
 
